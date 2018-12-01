2e79b073b693d084f88dbfbd04abb4277fdc5590
Projudah <projudah@gmail.com>
2018-12-01 15:37:00 -0500

    now parses messages to a dictionary

--- contentScript.js
+++ contentScript.js
@@ -26,15 +26,47 @@ function ScrollMax(maxscrolls){
 	}, 1000)
 	setTimeout(function(){
 		clearTimeout(inter)
-		text = scrollarea.innerText
-		console.log(text)
+		text = scrollarea.innerHTML
+		parse_text(scrollarea)
 	}, 1000*maxscrolls)
 }
 
 function start(){
-	maxScrolls = 10
+	maxScrolls = 5
 	console.log("Max Scolls is "+maxScrolls)
 	ScrollMax(maxScrolls)
 }
+
+function parse_text(textDom){
+	messages = document.getElementById('js_1')
+	message_array = messages.getElementsByClassName('_1t_p')
+	theirs = []
+	mine = []
+	dict = {}
+	count = 0
+	for (var id = 0; id < message_array.length; id++) {
+
+		messagegroup = message_array[id].getElementsByClassName('_41ud')[0]
+		message_group_messages = messagegroup.getElementsByClassName('_58nk')
+		message_group_orientation = messagegroup.getElementsByClassName('_3058')[0].getAttribute('data-tooltip-position')
+
+		message_texts = []
+
+		for (var mes = 0; mes < message_group_messages.length; mes++){
+			message_texts.push(message_group_messages[mes].innerText)
+		}
+
+		if (message_group_orientation == "right"){
+			mine = message_texts
+			dict[count] = [mine,theirs]
+			count++
+		}else{
+			theirs = message_texts
+		}
+
+	}
+	console.log(dict)
+}
+
 
 start()
